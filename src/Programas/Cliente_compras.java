0e07e2baf9f81d24c30d07d43ae7b66780c3e581
WalterLLacza <you@example.com>
2025-06-09 20:57:24 -0500

    carga inicial

--- src/Programas/Cliente_compras.java
+++ src/Programas/Cliente_compras.java
@@ -8,7 +8,7 @@ public class Cliente_compras {
     public static void main(String[] args) {
         //declaracion de variables
         String cliente;
-        double compra1,compra2,compra3,tc,pc;
+        double compra1,compra2,compra3,totalcompras,productocompras;
         //creando el objeto de lectura
         Scanner lectura = new Scanner(System.in);
         //entrada de datos
@@ -21,10 +21,11 @@ public class Cliente_compras {
         System.out.print("Ingrese La Compra 3 :");
         compra3=lectura.nextDouble();
         //proceso de datos
-        tc=compra1+compra2+compra3;
-        pc=tc/3;
+        totalcompras=compra1+compra2+compra3;
+        productocompras=totalcompras/3;
         //salida de datos
-        System.out.println("EL MONTO TOTAL DE COMPRAS: "+tc);
-        System.out.println("EL PROMEDIO DE COMPRAS: "+pc);
+        System.out.println("EL MONTO TOTAL DE COMPRAS DE: "+cliente);
+        System.out.println("EL MONTO TOTAL DE COMPRAS ES: "+totalcompras);
+        System.out.println("EL PROMEDIO DE COMPRAS ES: "+productocompras);
     }//fin del metodo
 }//fin del programa
